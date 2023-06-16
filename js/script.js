f2a575452d61f163f800d85dd6a77b6259b555ce
Augusto Silva <oaugustosilvas@gmail.com>
2023-06-15 21:45:29 -0300

    Reconfigurado botão de add tarefa + iniciado botão de exclusão de tarefa.

--- js/script.js
+++ js/script.js
@@ -3,6 +3,7 @@ function addItemToList() {
     var textInput = document.getElementById("textInput"); //Captação do que foi escrito
     var itemList = document.getElementById("itemList"); //Captação da lista
     var newItem = document.createElement("li"); //Criação do novo item da lista
+    const i = 0;
 
     // Criando o input com o checkbox
     var checkboxIcon = document.createElement("input");
@@ -12,15 +13,25 @@ function addItemToList() {
 
     // Criando o label onde vai escrito o texto
     var itemText = document.createElement("label");
-    itemText.textContent = textInput.value; // Colocando o texto digitado como valor no item
+    itemText.textContent = capitalizeFirstLetter(textInput.value)+";"; // Colocando o texto digitado como valor no item
 
     // Colocando o texto e o checkbox no item
     newItem.appendChild(checkboxIcon);
     newItem.appendChild(itemText);
+    newItem.id = "item" + i;
   
 
     // Criando um item na lista com as características do li
     itemList.appendChild(newItem);
 
     textInput.value = ""; // Limpar o campo de entrada após adicionar o item
+    i++;
+}
+
+function capitalizeFirstLetter(str) {
+    return str.charAt(0).toUpperCase() + str.slice(1);
+}
+
+function closeItem() {
+
 }
